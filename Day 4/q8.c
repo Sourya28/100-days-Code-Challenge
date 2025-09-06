e25c833805c84f9ff8bae8e467fb142327a5873e
Sourya Pokhariya <pokhariyasourya28@gmail.com>
2025-09-06 12:20:32 +0530

    Update q8.c

--- Day 4/q8.c
+++ Day 4/q8.c
@@ -1,3 +1,5 @@
+//Write a program to find and display the sum of the first n natural numbers.
+
 #include <stdio.h>
 
 int main() {
@@ -14,4 +16,5 @@ int main() {
     printf("The sum of the first %d natural numbers is: %d\n", n, sum);
 
     return 0;
+
 }
